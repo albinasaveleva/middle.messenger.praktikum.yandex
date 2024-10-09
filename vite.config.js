013f283563d3e857c462fbbab9f280e7bd800b3f
albinasaveleva <savelevalbina@gmail.com>
2024-10-09 18:42:36 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,19 @@ import postcss from '@vituum/vite-plugin-postcss';
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        changeProfileInfoPage: 'src/pages/change-profile-info-page.html',
+        changeProfilePasswordPage: 'src/pages/change-profile-password-page.html',
+        chatPage: 'src/pages/chat-page.html',
+        errorPage: 'src/pages/error-page.html',
+        loginPage: 'src/pages/login-page.html',
+        nonFoundPage: 'src/pages/non-found-page.html',
+        profilePage: 'src/pages/profile-page.html',
+        signupPage: 'src/pages/signup-page.html',
+      },
+    },
   },
   server: {
     port: 3000,
