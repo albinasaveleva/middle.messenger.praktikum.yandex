4a15296368a18c6454034e4986f1c4ffb6e575cb
albinasaveleva <savelevalbina@gmail.com>
2024-11-28 12:45:53 +0300

    commit

--- index.ts
+++ index.ts
@@ -4,39 +4,36 @@ import { ChatPage, LoginPage, NonFoundPage, ProfilePage, SignupPage } from './sr
 import store from './src/store';
 import Router from './src/utils/router';
 
-const init = async() => {
-    try {
-        await userController.getUser();
-        await chatController.getChats();
-    } catch ({reason}) {
-        console.log(reason)
-    }
-
-    setInterval(() => {chatController.getChats()}, 15000)
-};
-await init();
-
 const router = new Router("#app");
-const {user} = store.getState();
-
 router
     .use('/', LoginPage)
     .use('/sign-up', SignupPage)
     .use('/messenger', ChatPage)
     .use('/settings', ProfilePage)
-    .start();
 
+try {
+    await userController.getUser();
+    await chatController.getChats();
+} catch ({reason}) {
+    console.log(reason)
+} finally {
+    setInterval(() => {chatController.getChats()}, 15000);
 
-if(user === null) {
-    if (window.location.pathname === '/sign-up') {
-        router.go('/sign-up');
+    const {user} = store.getState();
+
+    router.start();
+
+    if(user === null) {
+        if (window.location.pathname === '/sign-up') {
+            router.go('/sign-up');
+        } else {
+            router.go('/');
+        }
     } else {
-        router.go('/');
-    }
-} else {
-    if (window.location.pathname === '/settings') {
-        router.go('/settings');
-    } else {
-        router.go('/messenger');
+        if (window.location.pathname === '/settings') {
+            router.go('/settings');
+        } else {
+            router.go('/messenger');
+        }
     }
 }
